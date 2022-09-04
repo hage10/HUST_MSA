5d97d3616ba57b98d00bf14f26a331275cbd678f
hage10 <utngo007@gmail.com>
2022-09-04 22:36:07 +0700

    update components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <the-container/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TheContainer from './components/layout/TheContainer.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    TheContainer,
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  font-family: "GoogleSans-Regular";
+  font-size: 13px;
+}
+.icon-24{
+  height: 24px;
+  width: 24px;
+}
+.input-24{
+width: 100%;
+height: 32px;
+}
+.m-input {
+    border: 1px solid #BBBBBB;
+    border-radius: 2px;
+    padding: 0 12px 0 10px;
+    outline: none;
+    font-size: 13px;
+}
+
+.m-input:focus {
+    border: 1px solid #3f97e4;
+}
+
+@font-face {
+  font-family: "GoogleSans-Bold";
+  src: url("./assets/fonts/notosans-bold.3ede5aff.woff2") format("opentype");
+}
+
+@font-face {
+  font-family: "GoogleSans-Thin";
+  src: url("./assets/fonts/notosans-semibold.df3e6dc4.woff2") format("opentype");
+}
+
+@font-face {
+  font-family: "GoogleSans-Italic";
+  src: url("./assets/fonts/notosans-italic.1c1aaac9.woff2") format("opentype");
+}
+
+@font-face {
+  font-family: "GoogleSans-Regular";
+  src: url("./assets/fonts/notosans-regular.2cb88a13.woff2") format("opentype");
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,30 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import StudentDetail from './view/student/StudentDetail'
+import TheAssignment from './view/student/TheAssignment'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import mitt from "mitt";
+const emitter = mitt();
 
-createApp(App).mount('#app')
+const msarouters = [
+    { path: '/infostudent', component: StudentDetail},
+    { path: '/assignment' , component: TheAssignment},
+]
+// app.component("ThePopup", Popup);
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: msarouters,
+})
+
+library.add(fas, fab, far)
+const app = createApp(App);
+app.use(router);
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.config.globalProperties.emitter = emitter;
+app.mount('#app')  
